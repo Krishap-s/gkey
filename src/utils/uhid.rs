4fc32b65bf53c3690b06642a4310bf124ef885d4
Krishap-s <krishap.s.sreenivasan@gmail.com>
2024-11-21 20:45:22 +0530

    feat: fix looping of uhid io on would_block error

--- src/utils/uhid.rs
+++ src/utils/uhid.rs
@@ -5,6 +5,7 @@ use std::io;
 use std::os::fd::AsRawFd;
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
+use std::task::{ready, Context, Poll};
 
 use pin_project::pin_project;
 use tio::unix::AsyncFd;
@@ -12,7 +13,7 @@ use tio::unix::TryIoError;
 use tio::{AsyncRead, AsyncWrite};
 use tokio::io as tio;
 
-use uhid_virt::{CreateParams, InputEvent, StreamError, UHID_EVENT_SIZE};
+use uhid_virt::{CreateParams, InputEvent, OutputEvent, StreamError, UHID_EVENT_SIZE};
 
 pub enum UHIDErr {
     // Error trying to poll the file descriptor
@@ -25,68 +26,41 @@ pub enum UHIDErr {
     TryIOError(TryIoError),
 }
 
-// Taken from https://github.com/flukejones/uhid-virt/blob/master/src/uhid_device.rs and modified
-// for tokio
 #[pin_project]
 pub struct UHIDDevice<T: Read + Write + AsRawFd> {
     pub handle: AsyncFd<T>,
 }
 
-/// Character misc-device handle for a specific HID device
-impl<T: AsRawFd + Read + Write> UHIDDevice<T> {
-    /// Write a SetReportReply, only use in reponse to a read SetReport event
-    pub async fn write_set_report_reply(&mut self, id: u32, err: u16) -> Result<usize, UHIDErr> {
-        let mut guard = self
-            .handle
-            .writable_mut()
-            .await
-            .map_err(UHIDErr::GuardErr)?;
-        let event: [u8; UHID_EVENT_SIZE] = InputEvent::SetReportReply { id, err }.into();
-        let write_res = guard
-            .try_io(|handle| handle.get_mut().write(&event))
-            .map_err(UHIDErr::TryIOError)?;
-        write_res.map_err(UHIDErr::IOError)
-    }
-
-    /// Write a GetReportReply, only use in reponse to a read GetReport event
-    pub async fn write_get_report_reply(
-        &mut self,
-        id: u32,
-        err: u16,
-        data: Vec<u8>,
-    ) -> Result<usize, UHIDErr> {
-        let mut guard = self
-            .handle
-            .writable_mut()
-            .await
-            .map_err(UHIDErr::GuardErr)?;
-        let event: [u8; UHID_EVENT_SIZE] = InputEvent::GetReportReply { id, err, data }.into();
-        let write_res = guard
-            .try_io(|handle| handle.get_mut().write(&event))
-            .map_err(UHIDErr::TryIOError)?;
-        write_res.map_err(UHIDErr::IOError)
-    }
-}
-
-//The try_io function of the asyncfd guard has to be looped to in the case a would block error may
-//appear
 impl<T: Read + Write + AsRawFd> AsyncRead for UHIDDevice<T> {
     fn poll_read(
         self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
+        cx: &mut Context<'_>,
         buf: &mut tio::ReadBuf<'_>,
-    ) -> std::task::Poll<io::Result<()>> {
-        buf.set_filled(UHID_EVENT_SIZE);
-        let device = self.project();
-        let mut byte_data: [u8; UHID_EVENT_SIZE] = [0; UHID_EVENT_SIZE];
-        device.handle.poll_read_ready_mut(cx).map(|guard_result| {
-            guard_result.and_then(|mut guard| loop {
-                match guard.try_io(|handle| handle.get_mut().read(byte_data.borrow_mut())) {
-                    Ok(res) => break res.map(|_| ()),
-                    Err(_) => continue,
+    ) -> Poll<io::Result<()>> {
+        let this = self.project();
+        loop {
+            let mut guard = ready!(this.handle.poll_read_ready_mut(cx))?;
+
+            let mut bytes = [0u8; UHID_EVENT_SIZE];
+            match guard.try_io(|inner| inner.get_mut().read(&mut bytes)) {
+                Ok(Ok(_)) => {
+                    let event = OutputEvent::try_from(bytes)
+                        .unwrap_or_else(|_| panic!("Invalid data from UHID FD"));
+                    match event {
+                        OutputEvent::Output { data } => buf.put_slice(&data),
+                        _ => {
+                            return Poll::Ready(Err(io::Error::new(
+                                io::ErrorKind::InvalidData,
+                                "Invalid event type from UHID",
+                            )))
+                        }
+                    }
+                    return Poll::Ready(Ok(()));
                 }
-            })
-        })
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
     }
 }
 
@@ -96,46 +70,50 @@ impl<T: Read + Write + AsRawFd> AsyncWrite for UHIDDevice<T> {
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> std::task::Poll<Result<usize, io::Error>> {
-        let device = self.project();
-        device.handle.poll_write_ready_mut(cx).map(|guard_result| {
-            guard_result.and_then(|mut guard| loop {
-                match guard.try_io(|handle| handle.get_mut().write(buf)) {
-                    Ok(res) => break res,
-                    Err(_) => continue,
-                }
-            })
-        })
+        let this = self.project();
+        let mut data: [u8; UHID_EVENT_SIZE] = InputEvent::Input { data: buf }.into();
+        loop {
+            let mut guard = ready!(this.handle.poll_read_ready_mut(cx))?;
+
+            match guard.try_io(|inner| inner.get_mut().write(&mut data)) {
+                Ok(Ok(size)) => return Poll::Ready(Ok(size)),
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
     }
 
     fn poll_flush(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), io::Error>> {
-        let device = self.project();
-        device.handle.poll_write_ready_mut(cx).map(|guard_result| {
-            guard_result.and_then(|mut guard| loop {
-                match guard.try_io(|handle| handle.get_mut().flush()) {
-                    Ok(res) => break res,
-                    Err(_) => continue,
-                }
-            })
-        })
+        let this = self.project();
+        loop {
+            let mut guard = ready!(this.handle.poll_read_ready_mut(cx))?;
+
+            match guard.try_io(|inner| inner.get_mut().flush()) {
+                Ok(Ok(_)) => return Poll::Ready(Ok(())),
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
     }
 
     fn poll_shutdown(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), io::Error>> {
-        let event: [u8; UHID_EVENT_SIZE] = InputEvent::Destroy.into();
-        let device = self.project();
-        device.handle.poll_write_ready_mut(cx).map(|guard_result| {
-            guard_result.and_then(|mut guard| loop {
-                match guard.try_io(|handle| handle.get_mut().write(&event)) {
-                    Ok(res) => break res.map(|_| ()),
-                    Err(_) => continue,
-                }
-            })
-        })
+        let this = self.project();
+        let data: [u8; UHID_EVENT_SIZE] = InputEvent::Destroy.into();
+        loop {
+            let mut guard = ready!(this.handle.poll_read_ready_mut(cx))?;
+
+            match guard.try_io(|inner| inner.get_mut().write(&data)) {
+                Ok(Ok(_)) => return Poll::Ready(Ok(())),
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
     }
 }
 
@@ -158,14 +136,13 @@ impl UHIDDevice<File> {
 
         let mut handle = AsyncFd::new(fd)?;
         let event: [u8; UHID_EVENT_SIZE] = InputEvent::Create(params).into();
-
-        handle.writable_mut().await.and_then(|mut guard| loop {
-            match guard.try_io(|handle| handle.get_mut().write(&event)) {
-                Ok(res) => break res.map(|_| ()),
-                Err(_) => continue,
+        loop {
+            let mut guard = handle.writable_mut().await?;
+            match guard.try_io(|inner| inner.get_mut().write(&event)) {
+                Ok(Ok(_)) => return Ok(UHIDDevice { handle }),
+                Ok(Err(err)) => return Err(err),
+                Err(_would_block) => continue,
             }
-        })?;
-
-        Ok(UHIDDevice { handle })
+        }
     }
 }
